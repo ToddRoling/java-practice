49fd0cf31bd34229c1aa73164f25c282d24647a9
Todd Roling <Todd.Roling@gmail.com>
2023-01-02 19:01:15 -0700

    simple hacker rank problem

--- src/main/java/com/github/toddroling/java/interview/preperation/datastructures/array/ArraySolutions.java
+++ src/main/java/com/github/toddroling/java/interview/preperation/datastructures/array/ArraySolutions.java
@@ -1,8 +1,19 @@
 package com.github.toddroling.java.interview.preperation.datastructures.array;
 
 import java.util.HashMap;
+import java.util.List;
 
 final class ArraySolutions {
+
+    // My solution to https://www.hackerrank.com/challenges/simple-array-sum/problem
+    public static int simpleArraySum(List<Integer> ar) {
+        int result = 0;
+        for (int element : ar) {
+            result += element;
+        }
+        return result;
+    }
+
     // My solution to https://practice.geeksforgeeks.org/problems/first-element-to-occur-k-times5150/1
     public int firstElementKTime(final int[] a, final int n, final int k) {
         final HashMap<Integer, Integer> frequencyMap = new HashMap<>();
