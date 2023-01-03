567fa8cab0149a9b0d70954ddd21eadeaa2c4076
Todd Roling <Todd.Roling@gmail.com>
2023-01-02 18:34:21 -0700

    Starting HackerRank problems. Added a solution for a calendar challenge.

---
+++ src/main/java/com/github/toddroling/java/interview/preperation/date/CalendarSolutions.java
@@ -0,0 +1,16 @@
+package com.github.toddroling.java.interview.preperation.date;
+
+import java.util.Calendar;
+
+public class CalendarSolutions {
+
+    // My solution to https://www.hackerrank.com/challenges/java-date-and-time/problem
+    public static String findDay(int month, int day, int year) {
+        String[] weekDays = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+        Calendar calendar = Calendar.getInstance();
+        //noinspection MagicConstant
+        calendar.set(year, month - 1, day);
+        int dayOfWeekNumber = calendar.get(Calendar.DAY_OF_WEEK);
+        return weekDays[dayOfWeekNumber - 1];
+    }
+}
